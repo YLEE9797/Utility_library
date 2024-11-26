00e3c300df3beafea80bbe6f1caafbc807112d78
YLEE9797 <ddongsa123@naver.com>
2024-11-26 17:57:14 +0900

    Lamda 함수
    Lamda 함수 수정

---
+++ ClassLibrary1/Lamda.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilityLib
+{
+    internal class Lamda
+    {
+        Func<int,string> ECU_RST => statusCode =>
+        {
+            switch (statusCode)
+            {
+                case 0: return "OK";
+                case 1: return "NG";
+                case 2: return "Unused";
+                default: return "Unknown";
+            }
+        };
+
+       public void ConvertIntToHex(int value)
+        {
+
+        }
+    }
+}
---
+++ ClassLibrary1/Properties.cs
@@ -0,0 +1,43 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.PerformanceData;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilityLib
+{
+    internal class Properties
+    {
+
+
+        //모델리스트 일떄(모델 여러개일때)
+        List<Model> Model_List = new List<Model>(); 
+        public List<Model> ModelList { get { return Model_List; } }
+        //모델(단일 모델일때)
+        public string Model { get; private set; }
+        //LHD/RHD
+        public string Drv {  get; private set; }
+        //LH/RH
+        public string Positon { get; private set; }
+        //IMS,RELAX,WALKIN
+        public string Type { get; private set; }
+        //HEAT/VENT
+        public string HV { get; private set; }
+        //CUSHION
+        public string Cush { get; private set; }
+        //FOLD
+        public string Fold { get; private set; }
+        //LUMB
+        public string Lumb { get; private set; }
+        //WAY
+        public string Way { get; private set; }
+        //LIMIT 재검사 Count
+        public int LimitRestestCount { get; private set; }
+        //HEAT_VENT LIMIT 재검사 Count
+        public int HeatVentRetestCount { get; private set; }
+       
+       
+    }
+}
