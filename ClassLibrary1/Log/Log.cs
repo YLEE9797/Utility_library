01ffd7f5ecd243085eb68533981ca8bac62d66a1
YLEE9797 <ddongsa123@naver.com>
2024-12-03 10:54:53 +0900

    feature_7
    Seat_Inspection 클래스 및 Log 클래스 추가

---
+++ ClassLibrary1/Log/Log.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UtilityLib.Log
+{
+    public class Log
+    {
+        public string FilePath = Application.StartupPath +"\\"+DateTime.Now.ToString()+".txt";
+        StreamWriter writer;
+
+        public void LogInfo(string msg)
+        {
+            writer = new StreamWriter(FilePath);
+            if (!File.Exists(FilePath))
+            {
+                Directory.CreateDirectory(FilePath);
+            }
+            else
+            {
+                writer.WriteLine(string.Format("LogInfo--> {0} {1}"), DateTime.Now.ToString(), msg);
+            }
+        }
+
+        public void LogErr(string msg)
+        {
+            writer = new StreamWriter(FilePath);
+            if (!File.Exists(FilePath))
+            {
+                Directory.CreateDirectory(FilePath);
+            }
+            else
+            {
+                writer.WriteLine(string.Format("LogErr--> {0} {1}"), DateTime.Now.ToString(), msg);
+            }
+
+        }
+        public void WarnLog(string msg)
+        {
+            writer = new StreamWriter(FilePath);
+            if (!File.Exists(FilePath))
+            {
+                Directory.CreateDirectory(FilePath);
+            }
+            else
+            {
+                writer.WriteLine(string.Format("LogWarn--> {0} {1}"), DateTime.Now.ToString(), msg);
+            }
+
+        }
+    }
+}
---
+++ ClassLibrary1/Seat_Inspection/SensorProcess.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilityLib.Seat_Inspection
+{
+    public class SensorProcess
+    {
+
+        /// <summary>
+        /// Msg 변수
+        /// </summary>
+        public string msg = string.Empty;
+        public double[] Input;
+        public double[] OutPut;
+
+
+        /// <summary>
+        /// 온도가 설정된 범위내에 있는지 판단하는 함수
+        /// </summary>
+        /// <param name="temperature">온도값</param>
+        public string ValidTemperature(double temperature,double Max)
+        {
+            try
+            {
+               if(temperature< Max)
+                {
+                    msg = "안정된 범위입니다";
+                   
+                }
+                else if(temperature > Max)
+                {
+                    msg= "온도가 너무 높습니다";
+                }
+            }
+            catch(Exception ee)
+            {
+                msg = ee.Message;      
+            }
+            return msg;
+        }
+        /// <summary>
+        /// 데이터 노이즈 필터링 
+        ///이동 평균 필터는 간단하면서도 효과적인 노이즈 제거 방법입니다. 일정한 크기의 윈도우(window)를 사용하여 
+        ///데이터를 평균 내어 갑작스러운 변화(노이즈)를 줄입니다.
+        /// </summary>
+        /// <param name="data"></param>
+        public static List<double> MovingAverageFilter(List<double> data, int windowSize)
+        {
+            var filteredData = new List<double>();
+            for (int i = 0; i < data.Count; i++)
+            {
+                int start = Math.Max(0, i - windowSize + 1);
+                double average = data.Skip(start).Take(windowSize).Average();
+                filteredData.Add(average);
+            }
+            return filteredData;
+        }
+
+
+        /// <summary>
+        /// 합계 내는 함수
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public double Sum(double[] data)
+        {
+            double sum =0.0;
+            for(int i = 0; i < data.Length; i++)
+            {
+                data[i] += sum;
+            }
+            return sum;
+        }
+        
+    }
+}
